0d6dd9f23bc7f22df619ec56124116ee2b5ca2d9
陈龙 <anjiplus-chenlong>
2019-07-17 17:47:33 +0800

    ...

---
+++ fom/src/main/java/com/cl/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.cl.controller;
+
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName LoginController
+ * @Description 登录 修改密码等接口
+ * @Author 陈龙
+ * @Date 2019/7/17 14:42
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+@Api(description = "登录,修改密码等接口")
+public class LoginController {
+
+    /**
+     * 日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+}
---
+++ fom/src/main/java/com/cl/service/ILoginService.java
@@ -0,0 +1,13 @@
+package com.cl.service;
+
+/**
+ * @ClassName ILoginService
+ * @Description 登录 修改密码等接口service
+ * @Author 陈龙
+ * @Date 2019/7/17 15:16
+ * @Version 1.0
+ **/
+public interface ILoginService {
+
+
+}
