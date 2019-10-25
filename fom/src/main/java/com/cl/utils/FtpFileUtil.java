f3b05f8312c6e077ecc42c10c6703d3c857df156
cl1203 <chen/*.0>
2019-10-25 11:18:38 +0800

    文件上传修改

--- fom/src/main/java/com/cl/utils/FtpFileUtil.java
+++ fom/src/main/java/com/cl/utils/FtpFileUtil.java
@@ -12,46 +12,51 @@ import java.io.InputStream;
 
 public class FtpFileUtil {
 
-    private static final Logger LOG = LoggerFactory.getLogger(FtpFileUtil.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(FtpFileUtil.class);
 
     //ftp服务器ip地址
     private static final String FTP_ADDRESS = "120.24.207.170";
     //端口号
     private static final int FTP_PORT = 21;
     //用户名
-    private static final String FTP_USERNAME = "root";
+    private static final String FTP_USERNAME = "ftpuser";
     //密码
-    private static final String FTP_PASSWORD = "ChenLong1203";
+    private static final String FTP_PASSWORD = "ftpuser";
     //图片路径
-    private static final String FTP_BASEPATH = "/home/www/site/img";
+    private static String FTP_BASEPATH = "/home/www/site/img";
+
+    private static FTPClient ftp;
 
     public  static boolean uploadFile(MultipartFile file , String fileName ){
         boolean success = false;
-        FTPClient ftp = new FTPClient();
-        ftp.setControlEncoding("UTF-8");
+        ftp = new FTPClient();
+        //ftp.setControlEncoding("UTF-8");
         try {
             InputStream input = file.getInputStream();
             int reply;
             ftp.connect(FTP_ADDRESS, FTP_PORT);// 连接FTP服务器
             ftp.login(FTP_USERNAME, FTP_PASSWORD);// 登录
+            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
             reply = ftp.getReplyCode();
             if (!FTPReply.isPositiveCompletion(reply)) {
                 ftp.disconnect();
                 return success;
             }
-            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
-            ftp.makeDirectory(FTP_BASEPATH );// 不存在才会执行这行代码 创建
+            FTP_BASEPATH = new String(FTP_BASEPATH.getBytes("UTF-8") , "ISO-8859-1");
+            //ftp.makeDirectory(FTP_BASEPATH );// 不存在才会执行这行代码 创建
             success = ftp.changeWorkingDirectory(FTP_BASEPATH );//切换到path下的文件夹下
-            if(!success){
-                throw new BusinessException("切换目录失败!");
-            }
+            /*if(!success){
+                createDir(FTP_BASEPATH);
+                success = ftp.changeWorkingDirectory(FTP_BASEPATH);
+            }*/
             ftp.enterLocalPassiveMode();
-            boolean flag  = ftp.storeFile(new String(fileName.getBytes("UTF-8"),"iso-8859-1"),input);
-            LOG.info("upload file: " + flag);
+            fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+            boolean flag  = ftp.storeFile(fileName,input);
+            LOGGER.info("upload file: " + flag);
             input.close();
             ftp.logout();
             success = true;
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             if (ftp.isConnected()) {
@@ -64,4 +69,19 @@ public class FtpFileUtil {
         return success;
     }
 
+
+    /**
+     * 目标服务器创建目录
+     * @param dirname
+     * @throws IOException
+     */
+    /*public static void createDir(String dirname) throws Exception{
+        boolean flag = false;
+        flag = ftp.changeWorkingDirectory(dirname);
+        if(flag){
+            throw new BusinessException("目录已存在！");
+        }
+        boolean createDir = ftp.makeDirectory(dirname);
+        LOGGER.info("在目标服务器上创建文件夹: " + dirname + "," + createDir);
+    }*/
 }
